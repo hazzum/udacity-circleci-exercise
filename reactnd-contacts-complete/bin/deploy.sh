20e4ce2d6ed9236aacc90a7ef9cc54c0a2d33a38
hazzum <hazem.mix2012@gmail.com>
2022-10-31 21:08:11 +0200

    CI steps

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,1 +1,4 @@
-aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+#!/bin/bash
+aws s3api create-bucket --bucket my-564345-bucket --region us-east-1 \
+&& aws s3 cp --recursive --acl public-read ./build s3://my-564345-bucket/ \
+&& aws s3 website s3://my-564345-bucket/ --index-document index.html
